ec67c7799ae48d852ffbdb9b8cea9f3a0d2da3f8
Tobias S <>
2021-04-11 17:38:05 +0000

    Add keysBonus() method in case a collision occurs to get all keys / hashtable blueprint

--- hashTables/blueprint.js
+++ hashTables/blueprint.js
@@ -53,27 +53,56 @@ class HashTable {
      }
      return keysArr
   }
+
+  // Bonus keys() without collision
+
+keysBonus() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
+  }
 }
 
+
+ 
 // this.data ==> [[key, value],[],[],[],[]....]
 
 
 const myHashTable = new HashTable(50);
 
-myHashTable.set("grapes", 1000);
-myHashTable.set("grapess", 1000);
-myHashTable.set("apples", 4000);
+myHashTable.set("grapes", 1000); // collision | bananas
+myHashTable.set("oranges", 1000);
+myHashTable.set("apples", 4000); 
+myHashTable.set("peaches", 4000);
+myHashTable.set("bananas", 4000); //collision | grapes
 // myHashTable.get('grapes')
 
 
 // test:
-// console.log(myHashTable);
+// console.log(myHashTable.data);
 
 // Get test
 // console.log(myHashTable.get("apples"));
 // console.log(myHashTable.get("grapes"));
-// console.log(myHashTable.get('grapess'));
+// console.log(myHashTable.get('oranges'));
 
 
 // Keys test: 
-myHashTable.keys()
+myHashTable.keys() // not including collision keys
+// myHashTable.keysBonus() // ** including collision keys
